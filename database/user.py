8354cb2294042e842876d966f31036399e05ecfe
molivramento <molgames@protonmail.com>
2022-04-03 21:17:37 -0300

    create project

---
+++ database/user.py
@@ -0,0 +1,11 @@
+
+from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+
+class Tools(Base):
+    __tablename__ = 'tools'
+
+    id = Column(Integer, primary_key=True)
